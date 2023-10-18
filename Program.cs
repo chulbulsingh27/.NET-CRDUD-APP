fdf4ab681d676c377ec9c6cd46d5c92448dcaa3f
chulbul singh <cs845418@gmail.com>
2023-10-18 11:19:48 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using fullStack.Models;
+
+namespace fullStack
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<EmployeeContext>(item => item.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+           
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
