9d3c54a6e698cbd66611fc3166ed35fa045998a4
chulbul singh <cs845418@gmail.com>
2023-10-19 18:30:39 +0530

    css added css added

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Crud from './Crud';
 function App() {
   return (
-    <div className="App">
-      <Crud/>
+    <div className='App' >
+      <Crud />
     </div>
   );
 }
--- src/Crud.jsx
+++ src/Crud.jsx
@@ -3,6 +3,9 @@ import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import {FiEdit} from "react-icons/fi"
+import {MdDeleteOutline} from "react-icons/md"
+import './Crud.css'
 
 import Modal from "react-bootstrap/Modal";
 import Container from "react-bootstrap/Container";
@@ -24,23 +27,23 @@ const Crud = () => {
   const [editName, setEditName] = useState("");
   const [editEmail, setEditEmail] = useState("");
 
-  const empData = [
-    {
-      EmployeeId: 1,
-      Name: "chulbul",
-      Email: "cs@gmail.com",
-    },
-    {
-      EmployeeId: 2,
-      Name: "rahul",
-      Email: "rahul@gmail.com",
-    },
-    {
-      EmployeeId: 3,
-      Name: "arul",
-      Email: "arul@gmail.com",
-    },
-  ];
+  // const empData = [
+  //   {
+  //     EmployeeId: 1,
+  //     Name: "chulbul",
+  //     Email: "cs@gmail.com",
+  //   },
+  //   {
+  //     EmployeeId: 2,
+  //     Name: "rahul",
+  //     Email: "rahul@gmail.com",
+  //   },
+  //   {
+  //     EmployeeId: 3,
+  //     Name: "arul",
+  //     Email: "arul@gmail.com",
+  //   },
+  // ];
   useEffect(() => {
     getData();
   }, []);
@@ -70,10 +73,10 @@ const Crud = () => {
   };
 
   const handleDelete = (id) => {
-    if (window.confirm("Are you sure to delete this Employee") == true) {
+    if (window.confirm("Are you sure to delete this Employee") === true) {
       axios.delete(`https://localhost:7201/api/Employee/${id}`)
       .then((result) => {
-        if(result.status == 200){
+        if(result.status === 200){
             toast.success('Employee has been deleteed successfully')
             getData();
         }
@@ -112,7 +115,9 @@ const Crud = () => {
     .then((result) => {
         getData();
         cleanData();
-        toast.success("Employee has been added successfully")
+        toast.success('Employee has been deleted successfully', {
+          className: 'custom-toast-success',
+        });
     }).catch((error) => {
         toast.error(error)
     })
@@ -131,8 +136,8 @@ const Crud = () => {
     <>
     <ToastContainer/>
       <Container style={{marginTop:"30px"}}>
-        <Row>
-          <Col>
+        <Row style={{backgroundColor:"#7416CC",borderRadius:"5px"}}>
+          <Col style={{padding:"20px",margin:"5px"}}>
             <input
               type="text"
               className="form-control "
@@ -142,7 +147,7 @@ const Crud = () => {
               onChange={(e) => setName(e.target.value)}
             />
           </Col>
-          <Col>
+          <Col style={{padding:"20px",margin:"5px"}}>
             <input
               type="email"
               className="form-control"
@@ -152,7 +157,7 @@ const Crud = () => {
               onChange={(e) => setEmail(e.target.value)}
             />
           </Col>
-          <Col>
+          <Col style={{padding:"20px",margin:"5px"}}>
             <button
               type="button"
               className="btn btn-primary"
@@ -166,47 +171,54 @@ const Crud = () => {
       <br />
       <Table striped bordered hover >
         <thead >
-          <tr>
-            <th>#</th>
-            <th>EmployeeId</th>
-            <th>Name</th>
-            <th>Email</th>
+          <tr style={{backgroundColor:"red"}}>
+            <th style={{backgroundColor:"gray"}}>#</th>
+            <th style={{backgroundColor:"gray"}}>EmployeeId</th>
+            <th style={{backgroundColor:"gray"}}>Name</th>
+            <th style={{backgroundColor:"gray"}}>Email</th>
+            <th style={{backgroundColor:"gray"}}>Actions</th>
           </tr>
         </thead>
         <tbody>
           {data && data.length > 0
             ? data.map((ele, index) => {
                 return (
-                  <tr key={index}>
+                  <tr key={index} style={{ backgroundColor: index % 2 === 0 ? "#8BAC9B" : "#A0736A" }}>
                     <td>{index + 1}</td>
-                    <td>{ele.id}</td>
-                    <td>{ele.name}</td>
-                    <td>{ele.email}</td>
-                    <td colSpan={2} style={{marginLeft:"15px"}}>
-                      <Button
+                    <td style={{ backgroundColor: "#8BAC9B" }}>{ele.id}</td>
+                    <td style={{ backgroundColor: "#A0736A" }}>{ele.name}</td>
+                    <td style={{ backgroundColor: "#A0736A" }}> {ele.email}</td>
+                    {/* <td  > */}
+                      {/* <Button
                         variant="primary"
                         onClick={() => handleEdit(ele.id)}
                       >
                         Edit
-                      </Button>
-                      <Button
+                      </Button> */}
+                      <button onClick={() => handleEdit(ele.id)}>
+                        <FiEdit size={25} style={{backgroundColor:"#8BAC9B"}}/>
+                      </button>
+                      {/* <Button
                         variant="danger"
                         onClick={() => handleDelete(ele.id)}
                       >
                         delete
-                      </Button>
-                    </td>
+                      </Button> */}
+                      <button onClick={() => handleDelete(ele.id)}>
+                        <MdDeleteOutline size={25} style={{backgroundColor:"#A0736A"}}/>
+                      </button>
+                    {/* </td> */}
                   </tr>
                 );
               })
             : "Loading....."}
         </tbody>
       </Table>
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={show} onHide={handleClose}  style={{backgroundColor:""}}>
         <Modal.Header closeButton>
-          <Modal.Title>Modify/Update Employee</Modal.Title>
+          <Modal.Title style={{backgroundColor:"#6c499e",color:"white"}}>Modify/Update Employee</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body style={{backgroundColor:"#6c499e",color:"white"}}>
           <Row>
             <Col>
               <input
